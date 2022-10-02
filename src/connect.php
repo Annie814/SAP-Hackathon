8570471cb6f66f016cfebed0b509394f59d611ce
Annie Wang <annie010814@gmail.com>
2022-10-01 19:13:54 -0700

    added auto create DB and DB tables

--- src/connect.php
+++ src/connect.php
@@ -7,24 +7,34 @@ function OpenCon()
     $dbpass = "root";
     $db = "ShareTable";
 
-
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $conn -> error);
 
 // Create database
-    $sql = "CREATE DATABASE ShareTable";
+    $sql = "CREATE DATABASE IF NOT EXISTS ShareTable";
     if ($conn->query($sql) === TRUE) {
         echo "Database created successfully";
+
     } else {
         echo "Error creating database: " . $conn->error;
     }
-    //echo "Connected Successfully";
 
+    $sql = file_get_contents('../sql/ShareTable.sql');
+
+
+    if ($conn->multi_query($sql) === TRUE) {
+        echo "Table MyGuests created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
     return $conn;
 }
+
+
 
 function CloseCon($conn)
 {
     $conn -> close();
 }
+
 
 ?>
