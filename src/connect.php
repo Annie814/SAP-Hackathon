b1356583292f679e7f802306871fc5f9ae26859a
Annie Wang <annie010814@gmail.com>
2022-10-01 18:43:24 -0700

    added auto create database in connect

--- src/connect.php
+++ src/connect.php
@@ -10,6 +10,13 @@ function OpenCon()
 
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n". $conn -> error);
 
+// Create database
+    $sql = "CREATE DATABASE ShareTable";
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
     //echo "Connected Successfully";
 
     return $conn;
